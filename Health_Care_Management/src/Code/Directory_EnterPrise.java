4b9b5f5e1dc1174ca773bac742ee86cbe3e483af
saicharan-myla <maila.s@northeastern.edu>
2022-04-23 13:24:43 -0400

    added hospital enterprise

--- Health_Care_Management/src/Code/Directory_EnterPrise.java
+++ Health_Care_Management/src/Code/Directory_EnterPrise.java
@@ -4,10 +4,54 @@
  */
 package Code;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author saich
  */
 public class Directory_EnterPrise {
     
+private ArrayList<EnterPrise> enterpList;
+
+
+public Directory_EnterPrise(){
+
+enterpList = new ArrayList();
+
+}
+
+    public ArrayList<EnterPrise> getEnterpList() {
+        return enterpList;
+    }
+
+
+  public EnterPrise AddEnterPrise(String name, EnterPrise.EntType type){
+  EnterPrise enterprise = null;
+ if (type == EnterPrise.EntType.Hospital){
+    enterprise = new HospitalEnterPrise(name);
+    enterpList.add(enterprise);
+     }
+
+else if (type == EnterPrise.EntType.Supplier){
+ enterprise = new SupplierEnterPrise(name);
+    enterpList.add(enterprise);
+
+}
+else if (type == EnterPrise.EntType.Logistics){
+ enterprise = new LogisticsEnterPrise(name);
+    enterpList.add(enterprise);
+}
+else if (type == EnterPrise.EntType.MedicalEquiPantry){
+ enterprise = new MedicalEquiPantryEnterPrise(name);
+    enterpList.add(enterprise);
+}
+else if (type == EnterPrise.EntType.Pharmacy){
+ enterprise = new PharmacyEnterPrise(name);
+    enterpList.add(enterprise);
+}
+
+return enterprise;
+}
+
 }
