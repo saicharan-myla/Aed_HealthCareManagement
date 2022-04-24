fc99e2688f6d73a0769cfcf83df7a8ba8bf9fa45
Prudhvi Nakkina <nakkina.p@northeastern.edu>
2022-04-24 01:35:07 -0400

    AED-project added equipment role - prudhvi

---
+++ Health_Care_Management/src/Code/Role/Role_MedicalEquipment.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Code.Role;
+
+import javax.swing.JPanel;
+import Code.Network;
+import Code.Organization.Organization;
+
+/**
+ *
+ * @author pridh
+ */
+public class Role_MedicalEquipment extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business,Network network) {
+        return new HealthcareEquipmentWorkAreaJPanel(userProcessContainer, business, account,  organization,network);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Medical Equipment Role";
+    }
+}
