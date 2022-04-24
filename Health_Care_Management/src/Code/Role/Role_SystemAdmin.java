7aa41999485604c08e9496714c78838d1b80c30a
Prudhvi Nakkina <nakkina.p@northeastern.edu>
2022-04-24 01:46:40 -0400

    AED-project added sys admin role - prudhvi

---
+++ Health_Care_Management/src/Code/Role/Role_SystemAdmin.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Code.Role;
+
+import Code.EnterPrise;
+import javax.swing.JPanel;
+import Code.Network;
+import Code.Organization.Organization;
+
+/**
+ *
+ * @author pridh
+ */
+public class Role_SystemAdmin extends Role {
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, EnterPrise enterprise, EcoSystem system,Network network) {
+        return new SystemAdminWorkAreaJPanel(userProcessContainer, system);
+    }
+
+    @Override
+    public String toString() {
+        return "Sys Admin Role";
+    }
+}
