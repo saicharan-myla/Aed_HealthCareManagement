6165efdf2af3bf1549a338562edfb8ac5bec4397
saicharan-myla <maila.s@northeastern.edu>
2022-04-30 11:18:42 -0400

    added system design

---
+++ Health_Care_Management/src/Code/Design/ConfigureASystem.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Code.Design;
+
+import Code.EcoSystem;
+import Code.Employee;
+import Code.Role.Role_SystemAdmin;
+
+/**
+ *
+ * @author saich
+ */
+public class ConfigureASystem {
+
+
+public static EcoSystem configure(){
+
+
+    EcoSystem eco = EcoSystem.getInstance();
+     Employee employee  = eco.getEmpDir().AddEmployee("NEU");
+      UserAccount ua = eco.getUserAccDir().createUserAccount("sps","sps", employee, new Role_SystemAdmin());
+return eco;
+
+}
+    
+}
