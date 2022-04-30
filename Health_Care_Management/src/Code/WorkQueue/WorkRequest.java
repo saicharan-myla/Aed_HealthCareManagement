4a7da8f75633e568b9ec9af14c2c46f55fe01c6e
Prudhvi Nakkina <nakkina.p@northeastern.edu>
2022-04-30 02:48:39 -0400

    AED - import fixes - prudhvi

--- Health_Care_Management/src/Code/WorkQueue/WorkRequest.java
+++ Health_Care_Management/src/Code/WorkQueue/WorkRequest.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Schema.WorkQueue;
+package Code.WorkQueue;
 
-import Schema.UserAccount.UserAccount;
+import Code.Account;
 import java.util.Date;
 
 
@@ -15,8 +15,8 @@ import java.util.Date;
  */
 public class WorkRequest {
     private String message;
-    private UserAccount sender;
-    private UserAccount receiver;
+    private Account sender;
+    private Account receiver;
     private String status;
     private String requestDate;
     private Date resolveDate;
@@ -29,19 +29,19 @@ public class WorkRequest {
         this.message = message;
     }
 
-    public UserAccount getSender() {
+    public Account getSender() {
         return sender;
     }
 
-    public void setSender(UserAccount sender) {
+    public void setSender(Account sender) {
         this.sender = sender;
     }
 
-    public UserAccount getReceiver() {
+    public Account getReceiver() {
         return receiver;
     }
 
-    public void setReceiver(UserAccount receiver) {
+    public void setReceiver(Account receiver) {
         this.receiver = receiver;
     }
 
